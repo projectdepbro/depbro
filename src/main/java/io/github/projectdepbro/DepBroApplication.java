247612a1fc39713571372882965d746f057e778a
Grig Alex <alexengrigdev@gmail.com>
2023-09-03 05:00:57 +0300

    Init project

---
+++ src/main/java/io/github/projectdepbro/DepBroApplication.java
@@ -0,0 +1,13 @@
+package io.github.projectdepbro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepBroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepBroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/projectdepbro/DepBroApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.projectdepbro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepBroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
