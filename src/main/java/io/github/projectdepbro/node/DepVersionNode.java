bb782fde15902a02855e01f25d4a4a44725aa601
Grig Alex <alexengrigdev@gmail.com>
2023-10-08 23:34:48 +0300

    Create DepArtifactNode and DepVersionNode

---
+++ src/main/java/io/github/projectdepbro/node/DepVersionNode.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2023 The Project DepBro Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.projectdepbro.node;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import java.util.Set;
+
+@Node("DepArtifact")
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepVersionNode {
+
+    @Id
+    private String versionId;
+
+    private String version;
+
+    @Relationship(type = "EXISTS_OF", direction = Relationship.Direction.OUTGOING)
+    private DepArtifactNode artifact;
+
+    @Relationship(type = "USED_IN", direction = Relationship.Direction.INCOMING)
+    private Set<DepVersionNode> dependencies;
+
+}
